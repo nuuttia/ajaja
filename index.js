ec90aa3b285afc01dadd53353e6c9adf5875b5ba
Nuutti Arvilommi <nuutti.arvilommi@aalto.fi>
2020-07-17 16:01:13 +0300

    nyt pls

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.get('/api/persons/:id', (request, response) => {
   if (person) {
     response.json(person)
   } else {
+    console.log('vituiks man')
     response.status(404).end()
   }
 })
